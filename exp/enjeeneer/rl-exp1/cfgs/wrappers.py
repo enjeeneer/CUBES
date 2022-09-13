e0ffa67e396799e28582bade070efb340ac63cbf
enjeeneer <scottjeen@gmail.com>
2022-09-13 18:42:04 -0400

    feat: wrapper for converting obs dict to array

---
+++ exp/enjeeneer/rl-exp1/cfgs/wrappers.py
@@ -0,0 +1,32 @@
+import gym
+import numpy as np
+
+
+def dict_to_array(obs):
+
+    # modify obs
+    vals = []
+    for _, value in obs.items():
+        vals.append(value)
+    obs = np.concatenate(vals, axis=0, dtype=np.float32)
+
+    return obs
+
+
+class ObsWrapper(gym.Wrapper):
+    def __init__(self, env):
+        super(ObsWrapper, self).__init__(env)
+        self.env = env
+
+    def step(self, action):
+        obs_dict, reward, done, truncated, info = self.env.step(action)
+
+        # modify obs
+        vals = []
+        for _, value in obs_dict.items():
+            vals.append(value)
+        obs_array = np.concatenate(vals, axis=0, dtype=np.float32)
+
+        return obs_array, reward, done, obs_dict, truncated, info
+
+
