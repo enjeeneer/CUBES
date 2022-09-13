da1a208f47f2766dd7e40e7b14d6f26da9cfe36e
enjeeneer <scottjeen@gmail.com>
2022-09-13 11:36:32 -0400

    fix: cfg parser and sac cfg

--- exp/enjeeneer/rl-exp1/cfgs/parser.py
+++ exp/enjeeneer/rl-exp1/cfgs/parser.py
@@ -5,10 +5,10 @@ def parse_cfg() -> Union[DictConfig, ListConfig]:
     """
     Parses agent and env configs files, adds c02 data and returns OmegaConf object
     """
-    agent_cfg_path = 'configs/sac.yaml'
-    env_cfg_path = 'configs/envs.yaml'
+    agent_cfg_path = 'cfgs/sac.yaml'
+    # env_cfg_path = 'configs/envs.yaml'
     base = OmegaConf.load(agent_cfg_path)
-    env = OmegaConf.load(env_cfg_path)
-    base.merge_with(env)
+    # env = OmegaConf.load(env_cfg_path)
+    # base.merge_with(env)
 
     return base
