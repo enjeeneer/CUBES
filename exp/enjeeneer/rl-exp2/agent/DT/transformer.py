7ace13a28b1d1ed9e40871d70cba1fa34fc9339c
enjeeneer <scottjeen@gmail.com>
2022-09-27 10:19:57 +0100

    feat: updated transformer implementation to match @OrigamiDream's

--- exp/enjeeneer/rl-exp2/agent/DT/transformer.py
+++ exp/enjeeneer/rl-exp2/agent/DT/transformer.py
@@ -1,273 +1,80 @@
-import pdb
-import math
 import torch
-import numpy as np
 import torch.nn as nn
-from torch.nn import functional as F
+from typing import Dict, Any, Union
 
-from .ein import EinLinear
 
-class CausalSelfAttention(nn.Module):
-    """
-    A multi-head masked self-attention module with projection as output. Taken from Janner (2021)
-    (https://github.com/jannerm/trajectory-transformer) who in turn mirrored Kaparthy (2020)
-    (https://github.com/karpathy/minGPT)
-    """
+class TransformerBlock(nn.Module):
+    def __init__(self, cfg: dict):
+        super(TransformerBlock, self).__init__()
 
-    def __init__(self, cfg):
-        super().__init__()
-        assert cfg.n_embd % cfg.n_head == 0
-        # key, query, value projections for all heads
-        self.key = nn.Linear(cfg.n_embd, cfg.n_embd)
-        self.query = nn.Linear(cfg.n_embd, cfg.n_embd)
-        self.value = nn.Linear(cfg.n_embd, cfg.n_embd)
+        self.cfg = cfg
+        self.attention = self.feed_forward = self.dropout = None
+        self.layer_norm1 = self.layer_norm2 = None
 
-        # regularization
-        self.attn_drop = nn.Dropout(cfg.attn_pdrop)
-        self.resid_drop = nn.Dropout(cfg.resid_pdrop)
-
-        # output projection
-        self.proj = nn.Linear(cfg.n_embd, cfg.n_embd)
-
-        # causal mask to ensure that attention is only applied to the left in the input sequence (triang. matrix of 1s)
-        self.register_buffer("mask", torch.tril(torch.ones(cfg.block_size, cfg.block_size))
-                                     .view(1, 1, cfg.block_size, cfg.block_size))
-
-        # mask previous value estimates
-        joined_dim = cfg.obs_dim + cfg.act_dim + 2
-        self.mask.squeeze()[:, joined_dim-1::joined_dim] = 0 # set every joined_dim(th) element to 0
-
-        self.n_head = cfg.n_head
-        self.n_embd = cfg.n_embd
-
-    def forward(self, x):
+    def build(self, input_shape):
         """
-        :param x: input tensor of shape (batch_size (B), sequence_length (T), embedding dimensionality (C))
-        :return:
+        Builds one transformer block.
         """
-        B, T, C = x.size() # batch size, sequence length, embedding dimensionality (n_embd)
+        input_dims = input_shape[-1] # need to confirm what this is
 
-        # calculate query, key, values for all heads in batch and move head forward to be the batch dim
-        ## [ B x n_heads x T x head_dim ]
-        k = self.key(x).view(B, T, self.n_head, C // self.n_head).transpose(1, 2) # (B, nh, T, hs)
-        q = self.query(x).view(B, T, self.n_head, C // self.n_head).transpose(1, 2) # (B, nh, T, hs)
-        v = self.value(x).view(B, T, self.n_head, C // self.n_head).transpose(1, 2) # (B, nh, T, hs)
+        # attention
+        self.attention = nn.MultiheadAttention(
+            embed_dim=self.cfg.embed_dim,
+            num_heads=self.cfg.num_att_heads,
+            dropout=self.cfg.dropout,
+            device=self.cfg.device)
 
-        # causal self-attention; Self-attend: (B, nh, T, hs) x (B, nh, hs, T) -> (B, nh, T, T). This gives us
-        # attention matrix of shape (sequence_length, sequence_length)
-        att = (q @ k.transpose(-2, -1)) * (1.0 / math.sqrt(k.size(-1)))
-        att = att.masked_fill(self.mask[:,:,:T,:T] == 0, float('-inf'))
-        att = F.softmax(att, dim=-1)
-        self._attn_map = att.clone()
-        att = self.attn_drop(att)
-        ## [ B x n_heads x T x head_size ]
-        y = att @ v # (B, nh, T, T) x (B, nh, T, hs) -> (B, nh, T, hs)
-        ## [ B x T x embedding_dim ]
-        y = y.transpose(1, 2).contiguous().view(B, T, C) # re-assemble all head outputs side by side
+        # attention dropout
+        self.dropout = nn.Dropout(self.cfg.dropout)
 
-        # output projection
-        y = self.resid_drop(self.proj(y))
-
-        return y
-
-class Block(nn.Module):
-    """
-    A transformer block i.e. the combination of self-attention, layer normalisation and linear layers.
-    """
-    def __init__(self, cfg):
-        super().__init__()
-        self.ln1 = nn.LayerNorm(cfg.n_embd)
-        self.ln2 = nn.LayerNorm(cfg.n_embd)
-        self.attn = CausalSelfAttention(cfg)
-        self.mlp = nn.Sequential(
-            nn.Linear(cfg.n_embd, 4 * cfg.n_embd), # 4x n_embd taken from GPT-3 paper
+        # feed forward
+        self.feed_forward = nn.Sequential(
+            nn.Linear(input_dims, self.cfg.hidden_dims),
             nn.GELU(),
-            nn.Linear(4 * cfg.n_embd, cfg.n_embd),
-            nn.Dropout(cfg.resid_pdrop),
+            nn.Dropout(self.cfg.dropout),
+            nn.Linear(self.cfg.hidden_dims, self.cfg.hidden_dims)
         )
 
-    def forward(self, x):
-        x = x + self.attn(self.ln1(x))
-        x = x + self.mlp(self.ln2(x))
+        # regularisation
+        self.layer_norm1 = nn.LayerNorm(normalized_shape=self.cfg.layer_norm_shape,
+                                        eps=1e-6)
+        self.layer_norm2 = nn.LayerNorm(normalized_shape=self.cfg.layer_norm_shape,
+                                        eps=1e-6)
+
+    def forward(self, inputs):
+        """
+        Passes tokensized embedding through
+        :param inputs:
+        :return:
+        """
+        residual = inputs
+        x = self.layer_norm1(inputs)
+        x = self.attention(x, x, x)
+        x = self.dropout(x)
+        x = x + residual
+
+        residual = x
+        x = self.layer_norm2(inputs)
+        x = self.feed_forward(x)
+        x = x + residual
+
         return x
 
-class GPT(nn.Module):
-    """GPT language model, with a context size of block_size """
 
-    def __init__(self, cfg):
-        super().__init__()
+class OutputPooler(nn.Module):
+    def __init__(self, cfg: dict):
+        super(OutputPooler, self).__init__()
+        self.cfg = cfg
 
-        # input embedding stem (+1 for stop token)
-        self.tok_emb = nn.Embedding(cfg.vocab_size * cfg.transition_dim + 1, cfg.n_embd)
+    def build(self):
+        self.outputs = nn.Sequential(
+            nn.Linear(self.cfg.hidden_dims, self.cfg.output_dims),
+            nn.Tanh()
+        )
 
-        self.pos_emb = nn.Parameter(torch.zeros(1, cfg.block_size, cfg.n_embd))
-        self.drop = nn.Dropout(cfg.embd_pdrop)
-        # transformer
-        self.blocks = nn.Sequential(*[Block(cfg) for _ in range(cfg.n_layer)])
-        # decoder head
-        self.ln_f = nn.LayerNorm(cfg.n_embd)
+    def forward(self, inputs):
+        x = torch.squeeze(inputs[:, 0:1, :], dim=1) # need to work out why we squeeze
+        x = self.outputs(x)
 
-        self.head = EinLinear(cfg.transition_dim, cfg.n_embd, cfg.vocab_size + 1, bias=False)
+        return x
 
-        self.vocab_size = cfg.vocab_size
-        self.stop_token = cfg.vocab_size * cfg.transition_dim
-        self.block_size = cfg.block_size
-        self.observation_dim = cfg.observation_dim
-
-        self.action_dim = cfg.action_dim
-        self.transition_dim = cfg.transition_dim # state_dim + act_dim + reward_dim + rtg_dim
-        self.action_weight = cfg.action_weight
-        self.reward_weight = cfg.reward_weight
-        self.value_weight = cfg.value_weight
-
-        self.embedding_dim = cfg.n_embd
-        self.apply(self._init_weights)
-
-    def get_block_size(self):
-        return self.block_size
-
-    def _init_weights(self, module):
-        if isinstance(module, (nn.Linear, nn.Embedding)):
-            module.weight.data.normal_(mean=0.0, std=0.02)
-            if isinstance(module, nn.Linear) and module.bias is not None:
-                module.bias.data.zero_()
-        elif isinstance(module, nn.LayerNorm):
-            module.bias.data.zero_()
-            module.weight.data.fill_(1.0)
-
-    def configure_optimizers(self, train_config):
-        """
-        This long function is unfortunately doing something very simple and is being very defensive:
-        We are separating out all parameters of the model into two buckets: those that will experience
-        weight decay for regularization and those that won't (biases, and layernorm/embedding weights).
-        We are then returning the PyTorch optimizer object.
-        """
-
-        # separate out all parameters to those that will and won't experience regularizing weight decay
-        decay = set()
-        no_decay = set()
-        whitelist_weight_modules = (torch.nn.Linear, EinLinear)
-        blacklist_weight_modules = (torch.nn.LayerNorm, torch.nn.Embedding)
-        for mn, m in self.named_modules():
-            for pn, p in m.named_parameters():
-                fpn = '%s.%s' % (mn, pn) if mn else pn # full param name
-
-                if pn.endswith('bias'):
-                    # all biases will not be decayed
-                    no_decay.add(fpn)
-                elif pn.endswith('weight') and isinstance(m, whitelist_weight_modules):
-                    # weights of whitelist modules will be weight decayed
-                    decay.add(fpn)
-                elif pn.endswith('weight') and isinstance(m, blacklist_weight_modules):
-                    # weights of blacklist modules will NOT be weight decayed
-                    no_decay.add(fpn)
-
-        # special case the position embedding parameter in the root GPT module as not decayed
-        no_decay.add('pos_emb')
-
-        # validate that we considered every parameter
-        param_dict = {pn: p for pn, p in self.named_parameters()}
-        inter_params = decay & no_decay
-        union_params = decay | no_decay
-        assert len(inter_params) == 0, "parameters %s made it into both decay/no_decay sets!" % (str(inter_params), )
-        assert len(param_dict.keys() - union_params) == 0, "parameters %s were not separated into either decay/no_decay set!" \
-                                                    % (str(param_dict.keys() - union_params), )
-
-        # create the pytorch optimizer object
-        optim_groups = [
-            {"params": [param_dict[pn] for pn in sorted(list(decay))], "weight_decay": train_config.weight_decay},
-            {"params": [param_dict[pn] for pn in sorted(list(no_decay))], "weight_decay": 0.0},
-        ]
-        optimizer = torch.optim.AdamW(optim_groups, lr=train_config.learning_rate, betas=train_config.betas)
-
-        return optimizer
-
-    def offset_tokens(self, idx):
-        _, t = idx.shape
-        n_states = int(np.ceil(t / self.transition_dim))
-        offsets = torch.arange(self.transition_dim) * self.vocab_size
-        offsets = offsets.repeat(n_states).to(idx.device)
-        offset_idx = idx + offsets[:t]
-        offset_idx[idx == self.vocab_size] = self.stop_token
-
-        return offset_idx
-
-    def pad_to_full_observation(self, x, verify=False):
-        b, t, _ = x.shape
-        n_pad = (self.transition_dim - t % self.transition_dim) % self.transition_dim
-        padding = torch.zeros(b, n_pad, self.embedding_dim, device=x.device)
-        ## [ B x T' x embedding_dim ]
-        x_pad = torch.cat([x, padding], dim=1)
-        ## [ (B * T' / transition_dim) x transition_dim x embedding_dim ]
-        x_pad = x_pad.view(-1, self.transition_dim, self.embedding_dim)
-        if verify:
-            self.verify(x, x_pad)
-
-        return x_pad, n_pad
-
-    def verify(self, x, x_pad):
-        b, t, embedding_dim = x.shape
-        n_states = int(np.ceil(t / self.transition_dim))
-        inds = torch.arange(0, self.transition_dim).repeat(n_states)[:t]
-        for i in range(self.transition_dim):
-            x_ = x[:, inds == i]
-            t_ = x_.shape[1]
-            x_pad_ = x_pad[:, i].view(b, n_states, embedding_dim)[:, :t_]
-            print(i, x_.shape, x_pad_.shape)
-            try:
-                assert (x_ == x_pad_).all()
-            except:
-                pdb.set_trace()
-
-    def forward(self, idx, targets=None, mask=None):
-        """
-            idx : [ B x T ]
-            values : [ B x 1 x 1 ]
-        """
-        b, t = idx.size()
-        assert t <= self.block_size,  f"Cannot forward sequence of length {t}, block size is only {self.block_size}"
-
-        offset_idx = self.offset_tokens(idx)
-        # [ B x T x embedding_dim ]
-        # forward the GPT model
-        token_embeddings = self.tok_emb(offset_idx)  # each index maps to a (learnable) vector
-        # [ 1 x T x embedding_dim ]
-        position_embeddings = self.pos_emb[:, :t, :]  # each position maps to a (learnable) vector
-        # [ B x T x embedding_dim ]
-        x = self.drop(token_embeddings + position_embeddings)
-        x = self.blocks(x)
-        # [ B x T x embedding_dim ]
-        x = self.ln_f(x)
-
-        # [ (B * T' / transition_dim) x transition_dim x embedding_dim ]
-        x_pad, n_pad = self.pad_to_full_observation(x)
-        # [ (B * T' / transition_dim) x transition_dim x (vocab_size + 1) ]
-        logits = self.head(x_pad)
-        # [ B x T' x (vocab_size + 1) ]
-        logits = logits.reshape(b, t + n_pad, self.vocab_size + 1)
-        # [ B x T x (vocab_size + 1) ]
-        logits = logits[:, :t]
-
-        # if we are given some desired targets also calculate the loss
-        if targets is not None:
-            loss = F.cross_entropy(logits.reshape(-1, logits.size(-1)), targets.view(-1), reduction='none')
-            if self.action_weight != 1 or self.reward_weight != 1 or self.value_weight != 1:
-                #### make weights
-                n_states = int(np.ceil(t / self.transition_dim))
-                weights = torch.cat([
-                    torch.ones(self.observation_dim, device=idx.device),
-                    torch.ones(self.action_dim, device=idx.device) * self.action_weight,
-                    torch.ones(1, device=idx.device) * self.reward_weight,
-                    torch.ones(1, device=idx.device) * self.value_weight,
-                ])
-                ## [ t + 1]
-                weights = weights.repeat(n_states)
-                ## [ b x t ]
-                weights = weights[1:].repeat(b, 1)
-                ####
-                loss = loss * weights.view(-1)
-            loss = (loss * mask.view(-1)).mean()
-        else:
-            loss = None
-
-        return logits, loss
