4eeb6f92e8cd7ae5bca1cea9214bd274ed3a9d74
enjeeneer <scottjeen@gmail.com>
2022-09-30 14:00:18 +0100

    fix: sac implementation

--- exp/enjeeneer/rl-exp1/cfgs/parser.py
+++ exp/enjeeneer/rl-exp1/cfgs/parser.py
@@ -6,9 +6,9 @@ def parse_cfg() -> Union[DictConfig, ListConfig]:
     Parses agent and env configs files, adds c02 data and returns OmegaConf object
     """
     agent_cfg_path = 'cfgs/sac.yaml'
-    # env_cfg_path = 'configs/envs.yaml'
+    env_cfg_path = 'cfgs/train.yaml'
     base = OmegaConf.load(agent_cfg_path)
-    # env = OmegaConf.load(env_cfg_path)
-    # base.merge_with(env)
+    env = OmegaConf.load(env_cfg_path)
+    base.merge_with(env)
 
     return base
