2d9636acf4a75f4ef2225a25cc9bc48ca1518138
enjeeneer <scottjeen@gmail.com>
2022-10-03 18:23:32 +0100

    fix: updated docstrings and code with better understanding of the shape of tensors being passed to embedding functions

--- exp/enjeeneer/rl-exp2/agent/DT/embeddings.py
+++ exp/enjeeneer/rl-exp2/agent/DT/embeddings.py
@@ -5,7 +5,7 @@ from typing import Dict, Union
 
 class DiscreteEmbedding(nn.Module):
 	"""
-	Function for embedding a discrete observation/action token.
+	Class for embedding a discrete observation/action token.
 	See Figure 13 in Gato paper.
 	"""
 	def __init__(self, cfg: Dict):
@@ -13,23 +13,28 @@ class DiscreteEmbedding(nn.Module):
 
 		self.cfg = cfg
 		self.embedding = nn.Embedding(
-			num_embeddings=self.cfg.emb_num,
-			embedding_dim=self.cfg.emb_dim,
-			device=self.cfg.device
+			num_embeddings=1024,  # number of bins in my discretisation (i.e. one-hot encoding)
+			embedding_dim=512,  # number of dimensions of embedded vector
+			device='cpu'
 		)
 
 	def forward(self, inputs):
 		"""
-		Forward pass of embedding function
+		Find embedding vector for a discretised obs/action vector.
+		Takes tensor of batches discretised tokens of shape (*, 1), but the value in the last dimenision
+		must be an int in the range [0, num_embeddings).
+
 		:param inputs: tensor of shape (?)
-		:return: embedding of shape (?)
+		:return: embedding tensor of shape (*, embedding_dim)
 		"""
 		return self.embedding(inputs)
 
 
 class PositionEncoding(nn.Module):
 	"""
-	Function for encoding the positon of an observation/action in a sequence.
+	Class for encoding the local-position of each observation / action dim. E.g. if an observation
+	has dimension 5, we find create a local postional embedding for each of the 5 dimensions, to signpost
+	where they sit in the observation. NB actions are always given the same positional encoding.
 	See Figure 16 in Gato paper.
 	"""
 	def __init__(self, cfg: Dict):
@@ -37,19 +42,27 @@ class PositionEncoding(nn.Module):
 
 		self.cfg = cfg
 		self.embedding = nn.Embedding(
-			num_embeddings=self.cfg.token_seq_length,
-			embedding_dim=self.cfg.layer_width,
+			num_embeddings=self.cfg.position_table_dim,  # larger value than any obs_dim
+			embedding_dim=self.cfg.embedding_dim,
 			device=self.cfg.device
 		)
 
-	def forward(self, inputs):
+	def forward(self, inputs, actions=False):
 		"""
 		Finds positional encoding input sequence. Figure 16 in Gato paper.
-		:param inputs: tensor of shape (batch_size, sequence_length, token_dim?)
-		:return:
+		Takes tensor of observations or actions and finds their local positional embedding. NB: actions
+		are always given the same positional embedding which we take, arbitrarily, as the last index in
+		the table look-up
+
+		:param inputs: embedding tensor of shape (*, *, obs_dim/act_dim, embedding_dim)
+		:return outputs: embedding tensor with positional encoding added of same shape as input
 		"""
-		input_size = torch.shape(inputs)[1]  # need to check this
-		pos = torch.range(start=0, end=input_size)
+		if actions:
+			pos = torch.tensor([self.cfg.position_table_dim], dtype=torch.int)  # every action gets last index in table
+		else:
+			input_size = torch.shape(inputs)[-2]  # 2nd to last dimension is number of dims in obs, last is embedding_dim
+			pos = torch.range(start=0, end=input_size)
+
 		outputs = inputs + self.embedding(pos)
 
 		return outputs
