036e4515ba146f75705b389a28c9efaaa8147ec4
enjeeneer <scottjeen@gmail.com>
2022-10-07 18:42:09 +0100

    checkpoint: friday night

--- exp/enjeeneer/rl-exp2/data/collector.py
+++ exp/enjeeneer/rl-exp2/data/collector.py
@@ -5,7 +5,7 @@ import bauwerk
 import numpy as np
 import pandas as pd
 from tqdm import tqdm
-from typing import TYPE_CHECKING, Optional, Tuple, Union, Dict
+from typing import Optional, Tuple, Dict
 from tokenizer import Tokenizer
 
 import sys
@@ -13,19 +13,19 @@ sys.path.append('../agent')
 from sac.agent import Agent
 
 sys.path.append('../utils')
-from utils import ObsWrapper, Cfg
-cfg = Cfg.parse()
+from utils import ObsWrapper
+
 
 class DataCollector:
     def __init__(self, cfg):
         super().__init__()
-        self.cfg = cfg
+        self.cfg = cfg.dataset
         self.eval_str = 'mean_eval_reward'
         self.build_dist_b = bauwerk.benchmarks.BuildDistB()
         self.cfg.save_dir = os.getcwd()
         self.tokenizer = Tokenizer(cfg=cfg.tokenizer)
 
-    def run(self, dir) -> None:
+    def run(self, dir: Optional[str] = None) -> None:
         """
         Performs training of models, rollouts, evals and saves associated transitions.
         """
@@ -33,7 +33,8 @@ class DataCollector:
             dir = self.cfg.save_dir
 
         # collect data and save
-        raw_data = self.collect()
+        raw_data = self.collect(optimal=self.cfg.optimal)
+        print('...saving raw data...')
         with open(os.path.join(dir, self.cfg.raw_name), 'wb') as f:
             pickle.dump(raw_data, f, protocol=pickle.HIGHEST_PROTOCOL)
 
@@ -41,12 +42,14 @@ class DataCollector:
         task_dict = self.tasks_dict(performative_data)
 
         # save task-wise data dict
+        print('...saving taskwise dictionary ...')
         with open(os.path.join(dir, self.cfg.dict_name), 'wb') as f:
             pickle.dump(task_dict, f, protocol=pickle.HIGHEST_PROTOCOL)
 
         # task-wise sequencing
         sequenced_dataset = {}
         for key, _ in task_dict.items():
+            # TODO: EVERYTHING IS GOOD UP UNTIL HERE
             trajs, obs_mask, act_mask, rew_mask = self.get_task_episodes(task_dict[key])  #
             input_sequences, target_sequencs, obs_masks, act_masks, rew_masks = self.get_sequenced_task_tokens(trajs,
                                                                                                                obs_mask,
@@ -63,15 +66,18 @@ class DataCollector:
 
             sequenced_dataset[key] = task_dict
 
+        print('...saving sequenced dataset...')
         with open(os.path.join(dir, self.cfg.seq_name), 'wb') as f:
             pickle.dump(sequenced_dataset, f, protocol=pickle.HIGHEST_PROTOCOL)
         
     def evaluate(self,
                  task,
+                 task_no,
                  agent: Optional[Agent] = None,
                  optimal_actions: Optional[np.array] = None) -> Tuple[float, pd.DataFrame]:
         """
-        Takes a task and one of Evaluates the mean stepwise performance of one rollout from our agent, and
+        Takes a task and either an agent or optimal sequence of actions from convex solver.
+        Evaluates the mean stepwise performance of one rollout from our agent, and
         returns the mean evaluation reward and transition data.
         """
 
@@ -87,7 +93,7 @@ class DataCollector:
 
         while not done:
             if optimal_actions:
-                action = np.array(optimal_actions[steps])
+                action = optimal_actions[0][steps]
             else:
                 action, _ = agent.act(obs, evaluate=True)
 
@@ -97,6 +103,7 @@ class DataCollector:
 
             # store data
             transition = {
+            self.cfg.task_id: task_no,
             'obs': obs,
             'action': action,
             'obs_': obs_,
@@ -121,7 +128,7 @@ class DataCollector:
         
         data = pd.DataFrame()
         build_dist_b = bauwerk.benchmarks.BuildDistB()
-        tasks = [build_dist_b.train_tasks[0]]
+        tasks = build_dist_b.train_tasks[:2]
 
         for j, task in enumerate(tasks):
             print('## Collecting Data for Bauwerk Task: {} ##'.format(j))
@@ -132,10 +139,8 @@ class DataCollector:
 
             if optimal:
                 optimal_actions = bauwerk.solve(env)
-                eval_reward, rollout = self.evaluate(task, optimal_actions)
+                eval_reward, rollout = self.evaluate(task, task_no=j, optimal_actions=optimal_actions)
                 data = pd.concat([data, rollout], ignore_index=True)
-
-                return data
 
             else:
                 env = ObsWrapper(env)
@@ -168,23 +173,25 @@ class DataCollector:
 
                         # evaluate and rollout
                         if agent.n_steps % self.cfg.eval_freq == 0:
-                            eval_reward, rollout = self.evaluate(task, agent)
+                            eval_reward, rollout = self.evaluate(task, task_no=j, agent=agent)
                             data = pd.concat([data, rollout], ignore_index=True)
 
                         obs = obs_
 
-                    return data
+        return data
     
     def get_performative(self, dataset: pd.DataFrame) -> pd.DataFrame:
         """
-        Takes dataset of obs, action, obs_, reward, done and cleans such that we only retain data from agent 
-        at >= threshold% of converged performance.
+        Takes dataset of tasks that include obs, action, obs_, reward, done and cleans such that we only retain data
+        from agent at >= threshold% of converged performance.
         """
-        
-        max_return = max(dataset[self.eval_str].unique())
-        threshold_return = max_return - np.absolute(max_return * (1 - self.cfg.threshold))
-
-        cleaned = dataset[dataset[self.eval_str] >= threshold_return]
+        cleaned = pd.DataFrame(columns=dataset.columns)
+        for task_id in dataset[self.cfg.task_id].unique():
+            sliced = dataset[dataset[self.cfg.task_id] == task_id]
+            max_return = max(sliced[self.eval_str].unique())
+            threshold_return = max_return - np.absolute(max_return * (1 - self.cfg.threshold))
+            task_data = sliced[sliced[self.eval_str] >= threshold_return]
+            cleaned = pd.concat([cleaned, task_data])
 
         return cleaned
 
@@ -194,13 +201,12 @@ class DataCollector:
         Each primary key in the dictionary represents a task.
         """
         # TODO: inherit keys and tasks from cfg
-        task_col = 'cfg'
         data_dict = {}
-        task_dict = {}
 
-        for i, task in enumerate(data[task_col].unique()):
-            task_data = data[data[task_col] == task]
-            task_dict['cfg'] = {**task_data.iloc[0]}  # TODO: this is likely wrong
+        for i, task in enumerate(data[self.cfg.task_id].unique()):
+            task_dict = {}
+            task_data = data[data[self.cfg.task_id] == task]
+            task_dict['cfg'] = task_data['cfg'].iloc[0]  # TODO: this is likely wrong
 
             obs_arr = task_data['obs'].to_numpy()
             obs_dim = task_data['obs'].iloc[0].shape[0]
@@ -234,8 +240,9 @@ class DataCollector:
         :return act_mask: array of action masks of shape (episodes, timesteps * (obs_dim + act_dim + rew_dim))
         :return rew_mask: array of reward masks of shape (episodes, timesteps * (obs_dim + act_dim + rew_dim))
         """
+        # TODO: i think something is going wrong in here re: data collection
         # get indexes of end of episodes
-        term_idx = np.where(data['0']['done'] == True)
+        term_idx = np.where(data['done'] == True)
         term_idx = np.insert(term_idx, 0, 0)
 
         obs_trajs = []
@@ -243,9 +250,9 @@ class DataCollector:
         rew_trajs = []
 
         for i in range(len(term_idx) - 1):
-            obs_traj = data['0']['obs_'][term_idx[i]: term_idx[i + 1], :]
-            act_traj = data['0']['action'][term_idx[i]: term_idx[i + 1], :]
-            reward_traj = data['0']['reward'][term_idx[i]: term_idx[i + 1], :]
+            obs_traj = data['obs_'][term_idx[i]: term_idx[i + 1], :]
+            act_traj = data['action'][term_idx[i]: term_idx[i + 1], :]
+            reward_traj = data['reward'][term_idx[i]: term_idx[i + 1], :]
             obs_trajs.append(obs_traj)
             act_trajs.append(act_traj)
             rew_trajs.append(reward_traj)
@@ -261,14 +268,12 @@ class DataCollector:
         padded_obs_trajs = np.zeros([num_trajs, max_traj, obs_trajs[0].shape[1]], dtype=np.float32)
         padded_act_trajs = np.zeros([num_trajs, max_traj, act_trajs[0].shape[1]], dtype=np.float32)
         padded_rew_trajs = np.zeros([num_trajs, max_traj, rew_trajs[0].shape[1]], dtype=np.float32)
-        early_term_trajs = np.zeros([num_trajs, max_traj, 1], dtype=np.bool)
 
         i = 0
         for obs, act, rew in zip(obs_trajs, act_trajs, rew_trajs):
             padded_obs_trajs[i, :traj_lengths[i], :] = obs  # [ep, timestep, obs_dim]
             padded_act_trajs[i, :traj_lengths[i], :] = act
             padded_rew_trajs[i, :traj_lengths[i], :] = rew
-            early_term_trajs[i, traj_lengths[i]:, :] = True
             i += 1
 
         # concat
@@ -296,7 +301,7 @@ class DataCollector:
                                         rew_mask: np.array) -> [np.array, np.array, np.array, np.array, np.array]:
         """
         Takes episode-length task trajectories and creates sequences of tokenized trajectories of length
-        context_size. We create both input and target trajectories for transformer training.
+        context_length. We create both input and target trajectories for transformer training.
         :param padded_trajs: traj array, shape [*, timesteps * (obs_dim, act_dim, rew_dim)]
         :param obs_mask:
         :param act_mask:
@@ -307,11 +312,11 @@ class DataCollector:
         :return rewards: array of action indices of shape [N, context_length]
         """
         # setup sequence array
-        input_sequences = np.empty(shape=(self.cfg.task_trajectories, self.cfg.context_size))
-        target_sequences = np.empty(shape=(self.cfg.task_trajectories, self.cfg.context_size))
-        obs = np.empty(shape=(self.cfg.task_trajectories, self.cfg.context_size))
-        actions = np.empty(shape=(self.cfg.task_trajectories, self.cfg.context_size))
-        rewards = np.empty(shape=(self.cfg.task_trajectories, self.cfg.context_size))
+        input_sequences = np.empty(shape=(self.cfg.task_trajectories, self.cfg.context_length))
+        target_sequences = np.empty(shape=(self.cfg.task_trajectories, self.cfg.context_length))
+        obs = np.empty(shape=(self.cfg.task_trajectories, self.cfg.context_length))
+        actions = np.empty(shape=(self.cfg.task_trajectories, self.cfg.context_length))
+        rewards = np.empty(shape=(self.cfg.task_trajectories, self.cfg.context_length))
 
         # tokenize
         token_trajs = self.tokenizer.tokenize(padded_trajs)
@@ -377,7 +382,3 @@ def batch(dataset: Dict, cfg) -> [np.array, np.array, np.array, np.array]:
                 rew_mask_batches[i, j, :] = dataset[task]['rew_masks'][seq_i, :]
 
     return input_batches, target_batches, obs_mask_batches, act_mask_batches, rew_mask_batches
-
-
-DC = DataCollector(cfg)
-DC.run()
