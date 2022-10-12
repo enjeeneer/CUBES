df21d4b256a40ec60a8494b66b5a88bc0eb2e437
enjeeneer <scottjeen@gmail.com>
2022-10-12 11:40:33 +0100

    fix: `predict_sequence()` docstring

--- exp/enjeeneer/rl-exp2/agent/DT/agent.py
+++ exp/enjeeneer/rl-exp2/agent/DT/agent.py
@@ -1,9 +1,11 @@
 from agent.DT.transformer import TransformerBlock, OutputPooler
 from agent.DT.embeddings import DiscreteEmbedding, PositionEncoding
+from data.tokenizer import Tokenizer
 
+import numpy as np
 import torch
 import torch.nn as nn
-from typing import Dict, Optional
+from typing import Dict, Optional, Union
 
 
 class Agent(nn.Module):
@@ -20,27 +22,32 @@ class Agent(nn.Module):
         self.output_pooler = OutputPooler(cfg=cfg.pooler)
         self.discrete_embedder = DiscreteEmbedding(cfg=cfg.embedding)
         self.positional_encoder = PositionEncoding(cfg=cfg.pos_encoder)
+        self.tokenizer = Tokenizer(cfg.tokenizer)
 
         # if cfg.stochastic_policy:
         #     self.actor = Actor(cfg.actor)
 
-    def predict_sequence(self, input_sequence: torch.tensor,
-                         obs_mask: torch.tensor,
-                         act_mask: torch.tensor,
-                         rew_mask: Optional[torch.tensor] = None,
+    def predict_sequence(self, input_sequence: Union[torch.tensor, np.array],
+                         obs_mask: Union[torch.tensor, np.array],
+                         act_mask: Union[torch.tensor, np.array],
+                         rew_mask: Optional[Union[torch.tensor, np.array]] = None,
                          targets: Optional[torch.tensor] = None):
         """
         Takes sequence, embeds, passes through transformer blocks and pools/
         :param input_sequence: tensor of inputs of shape [batch_dim, context_length]
-        :param obs_mask: tensor of inputs of shape [batch_dim, context_length]
-        :param act_mask: tensor of inputs of shape [batch_dim, context_length]
-        :param rew_mask: tensor of inputs of shape [batch_dim, context_length]
+        :param obs_mask: tensor of obs_dim positions in input sequence, shape [batch_dim, context_length]
+        :param act_mask: tensor of act_dim positions in input sequence, shape [batch_dim, context_length]
+        :param rew_mask: tensor of reward positions in input sequence, shape [batch_dim, context_length]
         :param targets: tensor of target variables, shape [sequence_length, batch_size, embed_dim]
         :return loss: loss tensor of shape [batch_size,]
-        :return output: output tensor of shape [sequence_length, batch_size] i.e. real-valued output
+        :return output: output array of shape [sequence_length, batch_size] i.e. real-valued output
         """
-        # TODO: In main.py, we'll probabably want a loop that makes action token predictions depending on the size of
-        #  act_dim, to confine the model to the correct output size.
+        if type(input_sequence) == np.ndarray:
+            input_sequence = torch.tensor(input_sequence, dtype=torch.int64)
+            obs_mask = torch.tensor(obs_mask, dtype=torch.int64)
+            act_mask = torch.tensor(act_mask, dtype=torch.int64)
+            if self.cfg.dataset.rewards:
+                rew_mask = torch.tensor(rew_mask, dtype=torch.int64)
 
         # embed sequence
         if self.cfg.dataset.rewards:
