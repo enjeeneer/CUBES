fix: normalisation in `ObsWrapper` now indexes via dict key

exp/enjeeneer/rl-exp2/utils/utils.py
@@ -1,5 +1,4 @@
 import gym
-import bauwerk
 import numpy as np
 from typing import Union
 from omegaconf import OmegaConf, DictConfig, ListConfig
@@ -9,18 +8,15 @@ class ObsWrapper(gym.Wrapper):
     def __init__(self, env):
         super(ObsWrapper, self).__init__(env)
         self.env = env
-        obs_low = []
-        obs_high = []
+        self.obs_low = {}
+        self.obs_high = {}
         for key, value in env.observation_space.items():
-            obs_low.append(env.observation_space[key].low)
-            obs_high.append(env.observation_space[key].high)
-
-        self.obs_low = np.concatenate(obs_low, axis=0)
-        self.obs_high = np.concatenate(obs_high, axis=0)
+            self.obs_low[key] = value.low
+            self.obs_high[key] = value.high
 
         # hack obs_low and obs_high until Arduin fixes bauwerk
-        self.obs_high[1] = 4.5
-        self.obs_high[2] = 3.5
+        self.obs_high['load'] = 4.5
+        self.obs_high['pv_gen'] = 3.5
 
     def step(self, action):
         """
@@ -32,12 +28,11 @@ class ObsWrapper(gym.Wrapper):
 
         # modify obs
         vals = []
-        for _, value in obs_dict.items():
-            vals.append(value)
-        obs_array = np.concatenate(vals, axis=0, dtype=np.float32)
+        for key, value in obs_dict.items():
+            val = (((value - self.obs_low[key]) / (self.obs_high[key] - self.obs_low[key])) * 2) - 1
+            vals.append(val)
 
-        # normalise
-        obs_array = ((obs_array - self.obs_low) / (self.obs_high - self.obs_low) * 2) - 1
+        obs_array = np.concatenate(vals, axis=0, dtype=np.float32)
 
         return obs_array, reward, done, info
 