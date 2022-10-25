ef79c7e687c31567e5cfb10307255516db555486
enjeeneer <scottjeen@gmail.com>
2022-10-25 21:41:30 +0100

    feat: skeleton `cubes` layout

---
+++ cubes/construct.py
@@ -0,0 +1,23 @@
+class Construct:
+    """class for constructing abitrary EnergyPlus sims"""
+    def __init__(self):
+        pass
+
+    def sample(self):
+        """samples house from ambience database"""
+        pass
+
+    def geometry(self):
+        """creates geometry for sampled house"""
+        pass
+
+    def buildup(self):
+        """adds build up to geometry"""
+        pass
+
+    def finish(self):
+        """adds remaining attriburtes from IDF file"""
+        pass
+
+    def core(self):
+        """all encompassing construction function"""
