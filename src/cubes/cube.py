b4d60582b39e66be970b3fd9b66728b7335709d1
enjeeneer <scottjeen@gmail.com>
2022-11-23 20:07:54 +0000

    feat: cube init

---
+++ src/cubes/cube.py
@@ -0,0 +1,40 @@
+"""
+Module containing a building control environment a.k.a. a cube.
+"""
+
+from dataclasses import dataclass
+
+
+@dataclass
+class EnvConfig:
+    """
+    Skeleton configuration for building env (cube).
+    Full definition TBC.
+    """
+
+    # general
+    timestep_len: float  # minutes
+
+    # geometry
+    roof_mat_thickness: float
+    roof_ins_thickness: float
+    floor_area: float
+    roof_pitch: float
+
+    # materials
+    insulation: str
+    concrete: str
+    brick: str
+    stone: str
+    wood: str
+
+    # equipment
+    heating: str  # e.g. ['heat_pump', 'boiler']
+
+    # sensors
+    sensor_no: int
+
+    # weather
+    location: str  # e.g. 'cambridge'
+
+
