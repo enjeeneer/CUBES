286a3e32a8928fa70c5efa6248a6199ea71d843c
HannesGauch <hannes.gauch@protonmail.com>
2022-12-15 16:11:55 +0000

    fix: don't  call base class constructors

--- src/cubes/cubesgym/simulators/custom_eplus_simulator.py
+++ src/cubes/cubesgym/simulators/custom_eplus_simulator.py
@@ -5,8 +5,12 @@ Class for connecting EnergyPlus with Python using Ptolomy server.
     Modified from sinergym to be able to use own input files
 """
 
-
+import os
+import socket
+import threading
 from typing import Any, Dict, List, Optional
+
+from sinergym.utils.logger import Logger
 
 from sinergym.simulators.eplus import EnergyPlus
 from cubes.cubesgym.utils.config import (
@@ -14,9 +18,15 @@ from cubes.cubesgym.utils.config import (
 )
 
 
+LOG_LEVEL_MAIN = "DEBUG"
+LOG_LEVEL_EPLS = "FATAL"
+LOG_FMT = "[%(asctime)s] %(name)s %(levelname)s:%(message)s"
+
+
 class EnergyPlusCustom(EnergyPlus):
+    # pylint: disable=super-init-not-called
     """
-    inergym.simulators.eplus.EnergPlus only modifying file paths
+    sinergym.simulators.eplus.EnergPlus only modifying file paths
     """
 
     def __init__(
@@ -49,18 +59,44 @@ class EnergyPlusCustom(EnergyPlus):
             config_params (Optional[Dict[str, Any]], optional):
                 Dictionary with all extra configuration for simulator. Defaults to None.
         """
-        super().__init__(
-            eplus_path,
-            weather_path,
-            bcvtb_path,
-            idf_path,
-            env_name,
-            variables,
-            act_repeat,
-            max_ep_data_store_num,
-            action_definition,
-            config_params,
+
+        self._env_name = env_name
+        self._thread_name = threading.current_thread().getName()
+        # pylint: disable=consider-using-f-string
+        self.logger_main = Logger().getLogger(
+            "EPLUS_ENV_%s_%s_ROOT" % (env_name, self._thread_name),
+            LOG_LEVEL_MAIN,
+            LOG_FMT,
         )
+
+        # Set the environment variable for bcvtb
+        os.environ["BCVTB_HOME"] = bcvtb_path
+        # Create a socket for communication with the EnergyPlus
+        self.logger_main.debug("Creating socket for communication...")
+        self._socket = socket.socket()
+        # Get local machine name
+        self._host = socket.gethostname()
+        # Bind to the host and any available port
+        self._socket.bind((self._host, 0))
+        # Get the port number
+        sockname = self._socket.getsockname()
+        self._port = sockname[1]
+        # Listen on request
+        self._socket.listen(60)
+        # pylint: disable=logging-not-lazy
+        self.logger_main.debug("Socket is listening on host %s port %d" % (sockname))
+
+        # Path attributes
+        self._eplus_path = eplus_path
+        self._weather_path = weather_path
+        self._idf_path = idf_path
+        # Episode existed
+        self._episode_existed = False
+
+        self._epi_num = 0
+        self._act_repeat = act_repeat
+        self._max_ep_data_store_num = max_ep_data_store_num
+        self._last_action = [0]
 
         # Creating models config (with extra params if exits)
         self._config = Config(
@@ -72,3 +108,53 @@ class EnergyPlusCustom(EnergyPlus):
             action_definition=action_definition,
             extra_config=config_params,
         )
+
+        # Annotate experiment path in simulator
+        self._env_working_dir_parent = self._config.experiment_path
+        # Setting an external interface if IDF building has not got.
+        self.logger_main.info(
+            "Updating idf ExternalInterface object if it is not present..."
+        )
+        self._config.set_external_interface()
+        # Updating IDF file (Location and DesignDays) with EPW file
+        self.logger_main.info(
+            "Updating idf Site:Location and SizingPeriod:DesignDay(s) "
+            "to weather and ddy file..."
+        )
+        self._config.adapt_idf_to_epw()
+        # Updating IDF file Output:Variables with observation variables
+        # specified in environment and variables.cfg construction
+        self.logger_main.info(
+            "Updating idf OutPut:Variable and variables XML tree model "
+            "for BVCTB connection."
+        )
+        self._config.adapt_variables_to_cfg_and_idf()
+        # Setting up extra configuration if exists
+        self.logger_main.info(
+            "Setting up extra configuration in building model if exists..."
+        )
+        self._config.apply_extra_conf()
+        # Setting up action definition automatic manipulation if exists
+        self.logger_main.info(
+            "Setting up action definition in building model if exists..."
+        )
+        self._config.adapt_idf_to_action_definition()
+
+        # In this lines Epm model is modified but no IDF is stored anywhere yet
+
+        # Eplus run info
+        (
+            self._eplus_run_st_mon,
+            self._eplus_run_st_day,
+            self._eplus_run_st_year,
+            self._eplus_run_ed_mon,
+            self._eplus_run_ed_day,
+            self._eplus_run_ed_year,
+            self._eplus_run_st_weekday,
+            self._eplus_n_steps_per_hour,
+        ) = self._config._get_eplus_run_info()
+
+        # Eplus one epi len
+        self._eplus_one_epi_len = self._config._get_one_epi_len()
+        # Stepsize in seconds
+        self._eplus_run_stepsize = 3600 / self._eplus_n_steps_per_hour
