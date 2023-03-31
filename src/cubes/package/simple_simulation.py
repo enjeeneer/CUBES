466425141c032c95d0e38ffca8087abc8e6deb92
Hannes Gauch <hannes.gauch@protonmail.com>
2023-03-31 16:52:32 +0000

    fix: no need to expand idf anymore

--- src/cubes/package/simple_simulation.py
+++ src/cubes/package/simple_simulation.py
@@ -1,12 +1,12 @@
 """Functions to package up IDF files with weather
 etc without creating a gym environment"""
 
-from cubes.package import weather, constants, utilities, variables
+from cubes.package import weather, constants, variables
 
 
 def prepare_simulation(idf, building_config, envconfig):
     idf = weather.get_weather_file_and_adapt_idf(idf, building_config)
-    idf = utilities.get_rdd_and_expand_idf(idf)
+    # idf = utilities.get_rdd_and_expand_idf(idf)
 
     # get observation variables
     observation_variables = variables.get_observation_variables(
