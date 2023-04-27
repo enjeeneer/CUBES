2514d3c0b342df8f1b9973d4b59c7ad51dbca027
enjeeneer <scottjeen@gmail.com>
2023-04-27 14:26:44 +0100

    base processor

--- src/cubes/data_processing/processors.py
+++ src/cubes/data_processing/processors.py
@@ -6,37 +6,18 @@ from pandas import DataFrame
 import numpy as np
 import abc
 import pathlib
-from config import ID_COLUMN, RESIDENTIAL_BUILDING_CODES, COMMON_FEATURES, COUNTRIES
+from cubes.data_processing.config import (
+    LOCATION_PATH,
+    GEOMETRY_PATH,
+    RESIDENTIAL_BUILDING_CODES,
+    COUNTRIES,
+)
 from cubes.construct.material import (
     NoMassMaterial,
     Material,
     WindowMaterialSimpleGlazing,
     WindowMaterialGlazing,
 )
-
-
-def get_common_features(
-    base_df_path: pathlib.Path,
-) -> DataFrame:
-    """
-    Returns common index for all building data. Taken
-    from ambience geometry data, which we assume to be
-    the most complete.
-    """
-
-    df = pd.read_excel(base_df_path)
-    df = df.set_index(ID_COLUMN)
-
-    # maintain only residential building codes
-    df = df[df["REFERENCE BUILDING USE CODE"].isin(RESIDENTIAL_BUILDING_CODES)]
-
-    # maintain reliable country data
-    df = df[df["REFERENCE BUILDING COUNTRY CODE"].isin(COUNTRIES)]
-
-    # maintain only common channels
-    df = df[COMMON_FEATURES]
-
-    return df
 
 
 class AbstractProcessor(metaclass=abc.ABCMeta):
@@ -76,11 +57,162 @@ class AbstractProcessor(metaclass=abc.ABCMeta):
     @property
     def id_column(self) -> str:
         """Column name for unique identifier."""
-        return ID_COLUMN
+        raise NotImplementedError
 
     def _load_raw_data(self, header: Optional[int] = 0) -> DataFrame:
         """Loads raw data."""
         return pd.read_excel(self.data_path, header=header)
+
+
+class BaseProcessor:
+    """Gets the base DataFrame that all data is merged onto."""
+
+    def __init__(
+        self,
+        location_df_path: pathlib.Path = LOCATION_PATH,
+        geometry_df_path: pathlib.Path = GEOMETRY_PATH,
+    ):
+        self._location_df_path = location_df_path
+        self._geometry_df_path = geometry_df_path
+
+    def __call__(self) -> DataFrame:
+        """
+        Returns base DataFrame for all building data. The base df creates
+        a common index out of the EU NUTS 3 location data (a quantification
+        of the number of dwellings in each NUTS 3 region) and the ambience
+        geometry data (a quantification of the building archetypes in each
+        country). We assume each NUTS 3 region contains every building
+        archetype described in the ambience dataset, with their number
+        proportional to the national fraction of each archetype in each
+        country.
+
+        Returns:
+            df: base DataFrame with index representing every archetype
+                for every NUTS 3 region.
+        """
+
+        location_df = pd.read_excel(self._location_df_path)
+        geometry_df = pd.read_excel(self._geometry_df_path)
+
+        # get proportions of archetypes for each country
+        geometry_df = self._calculate_archetype_proportions(geometry_df)
+        # get common df by combining NUTS 3 regions and building archetypes
+        df = self._get_common_df(location_df, geometry_df)
+
+        # merge location and geometry dfs into common df
+        merged_location = pd.merge(df, location_df, on="NUTS 3 REGION")
+        merged_location = merged_location.set_index(df.index)
+
+        geometry_df = geometry_df.set_index("REFERENCE BUILDING CODE")
+        fully_merged = pd.merge(
+            merged_location,
+            geometry_df,
+            left_on="REFERENCE BUILDING COUNTRY CODE",
+            right_index=True,
+        )
+
+        # get number of dwellings for each region/archetype pair
+        fully_merged["NUMBER OF DWELLINGS"] = (
+            fully_merged["COUNTRY ARCHETYPE PROPORTION"]
+            * fully_merged["Occupied conventional dwellings"]
+        )
+
+        return fully_merged
+
+    @staticmethod
+    def _get_common_df(location_df: DataFrame, geometry_df: DataFrame) -> DataFrame:
+        """
+        Gets common index for base df by combining NUTS 3 regions and
+        building archetypes.
+        Args:
+            location_df: DataFrame with location data.
+            geometry_df: DataFrame with building archetype data.
+        Returns:
+            common_index: Index representing every archetype for every NUTS 3 region.
+        """
+
+        df = pd.DataFrame()
+
+        # loop through each NUTS 3 region and get the associated country archetypes
+        for _, row in location_df.iterrows():
+            country = row["COUNTRY CODE"]
+            archetypes = geometry_df[
+                geometry_df["REFERENCE BUILDING COUNTRY CODE"] == country
+            ]["REFERENCE BUILDING CODE"].values
+
+            multi_index = pd.MultiIndex.from_product(
+                [[row["NUTS 3 REGION"]], archetypes],
+                names=["NUTS 3 REGION", "REFERENCE BUILDING CODE"],
+            )
+
+            region_df = pd.DataFrame(index=multi_index)
+
+            df = pd.concat([df, region_df], axis=0)
+
+        return df
+
+    def _calculate_archetype_proportions(self, geometry_df: DataFrame) -> DataFrame:
+        """
+        Calculates the proportion of each building archetype in each country.
+        Args:
+            geometry_df: DataFrame with building archetype data.
+        Returns:
+            df: DataFrame with building archetype proportions.
+        """
+        geometry_df = geometry_df.copy()
+
+        country_proportion = []
+
+        for country in COUNTRIES:
+            country = geometry_df[
+                geometry_df["REFERENCE BUILDING COUNTRY CODE"] == country
+            ]
+            total_dwellings = country[
+                "NUMBER OF REFERENCE BUILDINGS IN THE BUILDING STOCK SEGMENT"
+            ].sum()
+
+            for index in country.index:
+                archetype = country.loc[index]
+                proportion = (
+                    archetype[
+                        "NUMBER OF REFERENCE BUILDINGS IN THE BUILDING STOCK SEGMENT"
+                    ]
+                    / total_dwellings
+                )
+
+                country_proportion.append(proportion)
+
+        geometry_df["COUNTRY ARCHETYPE PROPORTION"] = country_proportion
+
+        return geometry_df
+
+
+class BuildingLocationProcessor(AbstractProcessor):
+    """Processes dwelling location data."""
+
+    def __init__(
+        self, features: List[str], data_path: pathlib.Path, common_features: DataFrame
+    ) -> None:
+        super().__init__(features, data_path=data_path, common_features=common_features)
+
+    def __call__(self) -> DataFrame:
+        """Loads raw data and cleans."""
+        df = pd.DataFrame(index=self.common_features.index)
+
+        loaded_df = self._load_raw_data()
+
+        try:
+            loaded_df = loaded_df[self.features]
+        except KeyError as e:
+            print(f"Raw location data does not have the required columns: {e}")
+
+        # set index to merge on
+        loaded_df = loaded_df.set_index(self.id_column)
+
+        # merge loaded df with common df
+        df = pd.concat([df, loaded_df], axis=1)
+
+        return df
 
 
 class GeometryProcessor(AbstractProcessor):
@@ -116,13 +248,74 @@ class GeometryProcessor(AbstractProcessor):
         loaded_df = self._calculate_window_to_wall_ratio(loaded_df)
         loaded_df = self._calculate_roof_to_floor_ratio(loaded_df)
 
+        # get proportion of each building code in each country
+        loaded_df = self._
+
         # set index to merge on
         loaded_df = loaded_df.set_index(self.id_column)
 
+        upsampled_df = self._upsample(loaded_df)
+
         # merge loaded df with common df
-        df = pd.concat([df, loaded_df], axis=1)
+        df = pd.concat([df, upsampled_df], axis=1)
 
         return df
+
+    def _calculate_code_proportions(self, loaded_df: DataFrame) -> DataFrame:
+        """
+        Upsamples the DataFrame to the spatial granularity of the common DataFrame.
+        """
+        proportions = pd.DataFrame(
+            index=COUNTRIES,
+            columns=[f"PROPORTION_{code}" for code in RESIDENTIAL_BUILDING_CODES],
+        )
+
+        apartment_block_proportions = []
+        multi_family_home_proportions = []
+        single_family_home_proportions = []
+        terrace_house_proportions = []
+
+        for country in COUNTRIES:
+            country = loaded_df[loaded_df["REFERENCE BUILDING COUNTRY CODE"] == country]
+            total_dwellings = country[
+                "NUMBER OF REFERENCE BUILDINGS IN THE BUILDING STOCK SEGMENT"
+            ].sum()
+
+            for code in RESIDENTIAL_BUILDING_CODES:
+                code_dwellings = country[
+                    "NUMBER OF REFERENCE BUILDINGS IN THE BUILDING STOCK SEGMENT"
+                ][country["REFERENCE BUILDING USE CODE"] == code].sum()
+                if code == "ABL":
+                    apartment_block_proportions.append(code_dwellings / total_dwellings)
+
+                elif code == "MFH":
+                    multi_family_home_proportions.append(
+                        code_dwellings / total_dwellings
+                    )
+
+                elif code == "SFH":
+                    single_family_home_proportions.append(
+                        code_dwellings / total_dwellings
+                    )
+
+                elif code == "TH":
+                    terrace_house_proportions.append(code_dwellings / total_dwellings)
+
+                else:
+                    raise ValueError(f"Unknown code {code}")
+
+        proportions["PROPORTION_ABL"] = apartment_block_proportions
+        proportions["PROPORTION_MFH"] = multi_family_home_proportions
+        proportions["PROPORTION_SFH"] = single_family_home_proportions
+        proportions["PROPORTION_TH"] = terrace_house_proportions
+
+        # merge into loaded_df
+        loaded_df = pd.merge(
+            loaded_df,
+            proportions,
+            left_on="REFERENCE BUILDING COUNTRY CODE",
+            right_index=True,
+        )
 
     @staticmethod
     def _calculate_window_to_wall_ratio(df: DataFrame) -> DataFrame:
