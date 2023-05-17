de9c860f2b373f1907018dc975a0adad7a8a5ea0
HannesGauch <hannes.gauch@protonmail.com>
2023-05-17 16:19:44 +0000

    fix: move function to utilities for reuse

--- src/cubes/construct/utilities.py
+++ src/cubes/construct/utilities.py
@@ -3,6 +3,14 @@
 import numpy as np
 
 
+def rotation_changes_north_direction(rotation):
+    """check if the building rotation is such that the north facing side is changed"""
+    if np.cos(rotation / 180 * np.pi) > 0:
+        return False
+    else:
+        return True
+
+
 def get_walls_in_limits(
     idf, x_lims=(-1e4, 1e4), y_lims=(-1e4, 1e4), z_lims=(-1e4, 1e4)
 ):
