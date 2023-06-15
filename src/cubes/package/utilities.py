77ca59a0e9ccc999c5c7b9dd9973359ee6d2ac3d
HannesGauch <hannes.gauch@protonmail.com>
2023-06-15 16:10:58 +0000

    add temperature forecast file generator

--- src/cubes/package/utilities.py
+++ src/cubes/package/utilities.py
@@ -1,9 +1,14 @@
 """collection of utilities for packaging up files for use with gym
 """
 from cubes.package import constants
+from cubes.constants import env_files_path
+from cubes.package.weather import get_weather_file_path
 from pathlib import Path
 import shutil
 from geomeppy import IDF
+import pandas as pd
+import numpy as np
+from typing import List
 
 
 def get_rdd_file(idf: IDF):
@@ -77,3 +82,47 @@ def check_observation_variables(obs_vars, rdd_vars) -> None:
             f"Observation variables: Variable called {obs_name}"
             " in observation variables is not valid for IDF building model",
         )
+
+
+def get_temperature_forecast_file_path(hours):
+    return env_files_path + "/temperature_forecast_" + str(hours) + "h.csv"
+
+
+def get_temperature_forecast_files(
+    weather_file_name: str, temperature_forecast_hours: List[int]
+):
+    """this function produces temperature forecast files
+    Numbers based on following assumptions:
+    - 92.5% of T forecasts for the next day lie within +-2 degC
+    - The standard deviation of the gaussian varies linearly with forecast time"""
+
+    if temperature_forecast_hours:
+        sigma_24h = 1.123302474060961  # gaussian based on Met office accuracy
+
+        def sigma(forecast_hours):
+            return sigma_24h / 24 * forecast_hours
+
+        temp_data = pd.read_csv(
+            get_weather_file_path(weather_file_name),
+            skiprows=8,
+            usecols=[6],
+            names=["T"],
+        )
+
+        for tfh in temperature_forecast_hours:
+            forecast = np.zeros(len(temp_data))
+
+            for i in range(len(temp_data)):
+                if i < len(temp_data) - tfh:
+                    forecast[i] = temp_data.loc[i + tfh, "T"] + np.random.normal(
+                        0, sigma(tfh), 1
+                    )
+                else:
+                    forecast[i] = temp_data.loc[i, "T"]
+
+            np.savetxt(
+                get_temperature_forecast_file_path(tfh),
+                forecast,
+                fmt="%10.2f",
+                newline=",\n",
+            )
