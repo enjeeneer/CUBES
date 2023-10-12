537af7f883e027611347e452951232e7984db497
enjeeneer <scottjeen@gmail.com>
2023-10-12 10:26:29 +0100

    wandb

--- agents/workspaces.py
+++ agents/workspaces.py
@@ -50,8 +50,8 @@ class SACWorkspace(AbstractWorkspace):
         torch.set_num_threads(1)
 
         run = wandb.init(
-            entity="hlg46",
-            project="leiden",
+            entity="enjeeneer",
+            project="liden",
             config=agent_config,
             tags=["hannes", "sac"],
             reinit=True,
