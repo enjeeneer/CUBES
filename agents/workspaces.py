4e352c58de635e035cd3446fbe64d18106cf624b
enjeeneer <scottjeen@gmail.com>
2023-10-16 14:25:45 +0100

    remove prints

--- agents/workspaces.py
+++ agents/workspaces.py
@@ -72,8 +72,6 @@ class SACWorkspace(AbstractWorkspace):
         done = True
 
         for i in tqdm(range(self.learning_steps)):
-            if i > 5:
-                break
 
             # reset env
             if done:
