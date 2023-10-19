a6290437ac9c318aa8cff06194661337281287fa
enjeeneer <scottjeen@gmail.com>
2023-10-19 18:02:35 +0100

    DT data collection workspace

--- agents/workspaces.py
+++ agents/workspaces.py
@@ -1,5 +1,6 @@
 # pylint: disable=invalid-name
 """Module that creates workspaces for training/evaling various agents."""
+import pandas as pd
 import torch
 
 import wandb
@@ -8,14 +9,14 @@ from loguru import logger
 from tqdm import tqdm
 import numpy as np
 from pathlib import Path
-from typing import Dict
+from typing import Dict, Tuple
 
 from agents.sac.agent import SoftActorCritic
 from agents.sac.replay_buffer import SoftActorCriticReplayBuffer
 from agents.base import AbstractWorkspace
 
 
-class SACWorkspace(AbstractWorkspace):
+class LeidenSACWorkspace(AbstractWorkspace):
     """
     Trains/evals/rollouts SAC on one task
     """
@@ -313,3 +314,226 @@ class SACWorkspace(AbstractWorkspace):
         }
 
         return metrics
+
+
+class DataCollectionWorkspace:
+    """
+    Trains/evals/rollouts SAC on one task.
+    """
+
+    def __init__(
+        self,
+        env,
+        learning_steps: int,
+        run_dir: Path,
+        eval_frequency: int,
+        eval_rollouts: int,
+        seed_steps: int,
+        wandb_logging: bool,
+        building_config: Dict,
+        performance_threshold: int,
+    ):
+        self.env = env
+        self.eval_frequency = eval_frequency  # how frequently to eval
+        self.eval_rollouts = eval_rollouts  # how many rollouts per eval step
+        self.run_dir = run_dir
+        self.learning_steps = learning_steps
+        self.seed_steps = seed_steps
+        self.wandb_logging = wandb_logging
+        self.building_config = building_config
+        self.eval_metric = "mean_reward"
+        self.performance_threshold = performance_threshold
+
+        self._STEPS_PER_DAY = 144
+
+    def train(
+        self,
+        agent: SoftActorCritic,
+        agent_config: Dict,
+        replay_buffer: SoftActorCriticReplayBuffer,
+    ):
+        """
+        Trains SAC on one task.
+        """
+        torch.set_num_threads(1)
+
+        dataset = pd.DataFrame()
+
+        if self.wandb_logging:
+            run = wandb.init(
+                entity="hannesg",
+                project="Leiden-paper",
+                config=agent_config,
+                tags=["sac"],
+                reinit=True,
+            )
+            model_path = self.run_dir / run.name
+
+        else:
+            model_path = self.run_dir / "local"
+
+        dataset_path = self.run_dir / "dataset.pickle"
+        makedirs(str(model_path), exist_ok=True)
+        makedirs(str(dataset_path), exist_ok=True)
+
+        logger.info("Training SAC.")
+        best_eval_reward = -1e8
+        done = True
+        self.eval_episode_no = 0
+
+        for i in tqdm(range(self.learning_steps)):
+
+            # reset env
+            if done:
+                obs = self.env.reset()
+            else:
+                obs = next_obs
+
+            # sample actions uniformly for seed steps
+            if i < self.seed_steps:
+                action = np.random.uniform(
+                    low=-1, high=1, size=(self.env.action_space.shape[0],)
+                )
+
+            else:
+                action = agent.act(
+                    obs,
+                    sample=True,
+                    replay_buffer=replay_buffer,
+                )
+            next_obs, reward, done, _ = self.env.step(action)
+
+            replay_buffer.add(
+                observation=obs,
+                action=action,
+                reward=reward,
+                next_observation=next_obs,
+                done=done,
+            )
+
+            eval_metrics = {}
+            if (i % self.eval_frequency == 0) & (i > 0):
+                eval_metrics, rollout = self.eval(
+                    agent=agent, replay_buffer=replay_buffer
+                )
+                self.eval_episode_no += 1
+
+                # store data
+                dataset = pd.concat([dataset, rollout], ignore_index=True)
+
+                if eval_metrics["eval/mean_episode_reward"] > best_eval_reward:
+                    logger.info(
+                        f"New max eval reward: {best_eval_reward:.3f} -> "
+                        f"{eval_metrics['eval/mean_episode_reward']:.3f}."
+                        f" Saving model."
+                    )
+
+                    name = f"sac_{i}.pickle"
+                    # save locally
+                    path = agent.save(model_path / name)
+                    # save to wandb
+                    if self.wandb_logging:
+                        run.save(path.as_posix(), base_path=model_path.as_posix())
+
+                    best_eval_reward = eval_metrics["eval/mean_episode_reward"]
+
+                agent.train()
+
+            train_metrics = {}
+            if (i % agent.actor_update_frequency == 0) and (i > self.seed_steps):
+                train_metrics = agent.update(replay_buffer=replay_buffer, step=i)
+
+            metrics = {**train_metrics, **eval_metrics}
+
+            if self.wandb_logging:
+                run.log(metrics)
+
+        # slice to only maintain performative dataset and save
+        dataset = self.get_performative(dataset)
+        dataset.to_pickle(dataset_path)
+
+        if self.wandb_logging:
+            run.finish()
+
+    def eval(
+        self, agent: SoftActorCritic, replay_buffer: SoftActorCriticReplayBuffer
+    ) -> Tuple[Dict, pd.DataFrame]:
+        """Performs eval rollouts."""
+        logger.info("Collecting eval rollout.")
+        rollout = pd.DataFrame()
+        agent.eval()
+
+        done = False
+        rollout_reward = []
+        rollout_violation_dt = {}
+
+        obs = self.env.reset()
+        while not done:
+            action = agent.act(
+                obs,
+                sample=False,
+                replay_buffer=replay_buffer,
+            )
+            obs_, reward, done, info = self.env.step(action)
+            rollout_reward.append(reward)
+
+            if not rollout_violation_dt:
+                for k, v in info["violation_delta_T"].items():
+                    rollout_violation_dt[k] = v / self._STEPS_PER_DAY
+            else:
+                for k, v in info["violation_delta_T"].items():
+                    rollout_violation_dt[k] += v / self._STEPS_PER_DAY
+
+            # store data
+            transition = {
+                "config": self.building_config,
+                "observation": obs,
+                "action": action,
+                "next_observation": obs_,
+                "reward": reward,
+                "done": done,
+                "episode": self.eval_episode_no,
+            }
+            transition = pd.DataFrame([transition])
+            rollout = pd.concat([rollout, transition], ignore_index=True)
+
+            obs = obs_
+
+        mean_reward = np.mean(rollout_reward)
+        for k, v in rollout_violation_dt.items():
+            rollout_violation_dt[k] = float(np.mean(v))
+
+        rollout[self.eval_metric] = mean_reward
+
+        metrics = {
+            "eval/mean_episode_reward": mean_reward,
+            "eval/mean_episode_violation_degree_days": rollout_violation_dt,
+        }
+
+        return metrics, rollout
+
+    def get_performative(self, dataset: pd.DataFrame) -> pd.DataFrame:
+        """
+        Takes dataset of tasks and cleans to retain data only data from agents
+        performing above some evaluation
+        threshold i.e. >= threshold% of converged performance.
+        Args:
+            dataset: DataFrame of transition data with columns
+                        [config, obs, action, obs_, reward,
+                        done, mean_reward, episode]
+        Returns:
+            sliced_data: Sliced DataFrame of high performing data.
+              The variables/columns are ['config', 'obs', 'action',
+              'obs_', 'reward', 'done', 'mean_reward']
+        """
+
+        sliced_dataset = pd.DataFrame(columns=dataset.columns)
+
+        max_return = max(dataset[self.eval_metric].unique())
+        threshold_return = max_return - np.absolute(
+            max_return * (1 - self.performance_threshold)
+        )
+        performative_data = dataset[dataset[self.eval_metric] >= threshold_return]
+        sliced_dataset = pd.concat([sliced_dataset, performative_data])
+
+        return sliced_dataset
