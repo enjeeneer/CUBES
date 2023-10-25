425ec76360e6b30183218cbd8424b4980f68ef8e
enjeeneer <scottjeen@gmail.com>
2023-10-25 10:46:24 +0100

    debugging

--- main_leiden.py
+++ main_leiden.py
@@ -234,7 +234,7 @@ else:
 
     elif args.algorithm == "rbc":
 
-        print("env action range", env.setpoints_space)
+        print("env action variables", env.variables["action"])
         agent = GeneralRBC(
             action_variable_names=env.variables["action"],
             action_ranges=env.setpoints_space,
