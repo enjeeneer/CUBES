f6046ece617c5a5d7cd39d061c00732a01627fdb
enjeeneer <scottjeen@gmail.com>
2023-10-26 11:15:55 +0100

    debug

--- cubes/package/core.py
+++ cubes/package/core.py
@@ -79,11 +79,12 @@ def register_environment(
         building_config,
         env_config,
     )
+    idf_file = str(env_config.files_dir / "building_model.idf")
     print("path", env_config.files_dir / "building_model.idf")
     print("string path", str(env_config.files_dir / "building_model.idf"))
-    print("type", type(str(env_config.files_dir / "building_model.idf")))
+    print("type", idf_file)
 
-    idf.save(filename=str(env_config.files_dir / "building_model.idf"))
+    idf.save(filename=idf_file)
 
     # register environment
     register(
