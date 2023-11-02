92ea5c6c1ed1a8f30b40e2a5abfff125ee7b8a5b
HannesGauch <hannes.gauch@protonmail.com>
2023-11-02 16:18:42 +0000

    more runs parallel

--- exp/hannes/Leiden-study/05_training/run_sac_parallel.sh
+++ exp/hannes/Leiden-study/05_training/run_sac_parallel.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-for c in {0..4}
+for c in {0..6}
 do
     rm -r "case_${c}"
     mkdir "case_${c}"
@@ -8,7 +8,7 @@ do
     cd ..
 done
 wait
-for c in {5..9}
+for c in {7..13}
 do
     rm -r "case_${c}"
     mkdir "case_${c}"
@@ -17,16 +17,7 @@ do
     cd ..
 done
 wait
-for c in {10..14}
-do
-    rm -r "case_${c}"
-    mkdir "case_${c}"
-    cd "case_${c}"
-    python3.9 ../../../../../main_leiden.py --case $c --year 2022 --rep 0 &
-    cd ..
-done
-wait
-for c in {15..19}
+for c in {14..19}
 do
     rm -r "case_${c}"
     mkdir "case_${c}"
