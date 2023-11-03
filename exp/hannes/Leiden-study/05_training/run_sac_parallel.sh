7922f6a9aeb9339f506319cc7edfd62316cabd5d
HannesGauch <hannes.gauch@protonmail.com>
2023-11-03 09:44:10 +0000

    only run two cases in parallel

--- exp/hannes/Leiden-study/05_training/run_sac_parallel.sh
+++ exp/hannes/Leiden-study/05_training/run_sac_parallel.sh
@@ -1,27 +1,13 @@
 #!/bin/bash
-for c in {0..6}
+for ((start=0; start<=18; start+=2));
 do
-    rm -r "case_${c}"
-    mkdir "case_${c}"
-    cd "case_${c}"
-    python3.9 ../../../../../main_leiden.py --case $c --year 2022 --rep 0 &
-    cd ..
-done
-wait
-for c in {7..13}
-do
-    rm -r "case_${c}"
-    mkdir "case_${c}"
-    cd "case_${c}"
-    python3.9 ../../../../../main_leiden.py --case $c --year 2022 --rep 0 &
-    cd ..
-done
-wait
-for c in {14..19}
-do
-    rm -r "case_${c}"
-    mkdir "case_${c}"
-    cd "case_${c}"
-    python3.9 ../../../../../main_leiden.py --case $c --year 2022 --rep 0 &
-    cd ..
+    for ((c=start;c<=start+1;c++));
+    do
+        rm -r "case_${c}"
+        mkdir "case_${c}"
+        cd "case_${c}"
+        python3.9 ../../../../../main_leiden.py --case $c --year 2022 --rep 0 &
+        cd ..
+    done
+    wait
 done
