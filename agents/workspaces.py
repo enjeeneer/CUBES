6f32a041099b015ff1cc2a8d1a2a560312837d88
enjeeneer <scottjeen@gmail.com>
2023-11-09 16:26:15 +0000

    printing

--- agents/workspaces.py
+++ agents/workspaces.py
@@ -642,6 +642,11 @@ class DecisionTransformerWorkspace(AbstractWorkspace):
 
             metrics = {**train_metrics, **val_metrics}
 
+            logger.info(
+                f"Train loss: {train_metrics['train/loss']:.2f}"
+                f" Val loss: {val_metrics['train/val_loss']:.2f}"
+            )
+
             if self.wandb_logging:
                 run.log(metrics)
 
