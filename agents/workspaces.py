8a0d787b8a6343ee6e72a82e69b34f85ac1f5651
enjeeneer <scottjeen@gmail.com>
2023-11-10 11:09:29 +0000

    printing float precision

--- agents/workspaces.py
+++ agents/workspaces.py
@@ -643,8 +643,8 @@ class DecisionTransformerWorkspace(AbstractWorkspace):
             metrics = {**train_metrics, **val_metrics}
 
             logger.info(
-                f"Train loss: {train_metrics['train/loss']:.2f} |"
-                f" Val loss: {val_metrics['train/val_loss']:.2f}"
+                f"Train loss: {train_metrics['train/loss']:.5f} |"
+                f" Val loss: {val_metrics['train/val_loss']:.5f}"
             )
 
             if self.wandb_logging:
