e8d75ba72de510bc0deb4ecb43db7558ea203056
HannesGauch <hannes.gauch@protonmail.com>
2023-11-14 17:16:37 +0000

    allow switching between lin and tolerance reward

--- cubes/package/envconfig.py
+++ cubes/package/envconfig.py
@@ -65,6 +65,7 @@ class EnvConfig:
     timesteps_per_hour: int = 6
 
     # reward
+    reward_function_type: str = "Linear"
     temp_range_comfort_winter: Tuple[int, int] = (20, np.inf)
     temp_range_comfort_summer: Tuple[int, int] = (20, np.inf)
     summer_start: Tuple[int, int] = (6, 1)
