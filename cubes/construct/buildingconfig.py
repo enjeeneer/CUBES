16cfcb2cf284bab30cd84235e375224234274a85
HannesGauch <hannes.gauch@protonmail.com>
2023-11-20 16:08:52 +0000

    allow operative temperature

--- cubes/construct/buildingconfig.py
+++ cubes/construct/buildingconfig.py
@@ -151,6 +151,7 @@ class BuildingConfig:
     grid_carbon_intensity_file_name: str
 
     # setpoint schedules
+    use_operative_temperature: float
     heating_setpoint: float
     heating_setback: float
     heating_setpoint_schedule: str
