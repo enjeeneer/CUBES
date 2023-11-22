72abc8d0348d901cdf3d5f18eeb7f070c873d803
HannesGauch <hannes.gauch@protonmail.com>
2023-11-22 16:59:47 +0000

    evaluate heating service

--- agents/workspaces.py
+++ agents/workspaces.py
@@ -161,6 +161,7 @@ class LeidenSACWorkspace(AbstractWorkspace):
         eval_ndt_t_violations = {}
         eval_ndt_aq_violations = {}
         eval_heating_dt = {}
+        eval_heating_service_dt = {}
         eval_heating_beyond_comf_dt = {}
         eval_violation_dt = {}
         eval_violation_daq = {}
@@ -176,6 +177,7 @@ class LeidenSACWorkspace(AbstractWorkspace):
             rollout_ndt_t_violations = {}
             rollout_ndt_aq_violations = {}
             rollout_heating_dt = {}
+            rollout_heating_service_dt = {}
             rollout_violation_daq = {}
             rollout_heating_beyond_comf_dt = {}
             rollout_violation_dt = {}
@@ -214,6 +216,13 @@ class LeidenSACWorkspace(AbstractWorkspace):
                 else:
                     for k, v in info["heating_delta_T"].items():
                         rollout_heating_dt[k] += v / 144
+
+                if not rollout_heating_service_dt:
+                    for k, v in info["heating_service"].items():
+                        rollout_heating_service_dt[k] = v / 144
+                else:
+                    for k, v in info["heating_service"].items():
+                        rollout_heating_service_dt[k] += v / 144
 
                 if not rollout_heating_beyond_comf_dt:
                     for k, v in info["heating_beyond_comf_delta_T"].items():
@@ -267,6 +276,13 @@ class LeidenSACWorkspace(AbstractWorkspace):
                 for k, v in rollout_heating_dt.items():
                     eval_heating_dt[k].append(v)
 
+            if not eval_heating_service_dt:
+                for k, v in rollout_heating_service_dt.items():
+                    eval_heating_service_dt[k] = [v]
+            else:
+                for k, v in rollout_heating_service_dt.items():
+                    eval_heating_service_dt[k].append(v)
+
             if not eval_heating_beyond_comf_dt:
                 for k, v in rollout_heating_beyond_comf_dt.items():
                     eval_heating_beyond_comf_dt[k] = [v]
@@ -301,6 +317,10 @@ class LeidenSACWorkspace(AbstractWorkspace):
         for k, v in eval_heating_dt.items():
             eval_heating_dt_means[k] = float(np.mean(v))
 
+        eval_heating_service_dt_means = {}
+        for k, v in eval_heating_service_dt.items():
+            eval_heating_service_dt_means[k] = float(np.mean(v))
+
         eval_heating_beyond_comf_dt_means = {}
         for k, v in eval_heating_beyond_comf_dt.items():
             eval_heating_beyond_comf_dt_means[k] = float(np.mean(v))
@@ -322,6 +342,8 @@ class LeidenSACWorkspace(AbstractWorkspace):
             "eval/mean_episode_ndt_t_violations": eval_t_violations_means,
             "eval/mean_episode_ndt_aq_violations": eval_aq_violations_means,
             "eval/mean_episode_heating_degree_days": eval_heating_dt_means,
+            "eval/mean_episode_heating_service_degree_days": (
+                eval_heating_service_dt_means),
             "eval/mean_episode_heating_beyond_comfort_degree_days": (
                 eval_heating_beyond_comf_dt_means
             ),
