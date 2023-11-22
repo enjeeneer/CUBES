a044a2db3a1a24975176116ddb5f2d254fe1efec
HannesGauch <hannes.gauch@protonmail.com>
2023-11-22 16:59:11 +0000

    introduce heating service in reward function

--- cubes/cubesgym/utils/rewards.py
+++ cubes/cubesgym/utils/rewards.py
@@ -560,6 +560,7 @@ class LinearRewardTEAQ(BaseReward):
             heating_delta_t,
             heating_beyond_comf_delta_t,
             violation_delta_t,
+            heating_service
         ) = self._get_comfort(obs_dict, old_obs_dict)
         reward_comfort = -self.lambda_temp * comfort
 
@@ -591,6 +592,7 @@ class LinearRewardTEAQ(BaseReward):
             "heating_beyond_comf_delta_T": heating_beyond_comf_delta_t,
             "violation_delta_T": violation_delta_t,
             "violation_delta_aq": violation_delta_aq,
+            "heating_service": heating_service
         }
 
         return reward, reward_terms
@@ -677,6 +679,7 @@ class LinearRewardTEAQ(BaseReward):
         violation_delta_t = {}
         heating_delta_t = {}
         heating_beyond_comf_delta_t = {}
+        heating_service = {}
         for o, t, z in zip(occs, temps, zones):
             supp = 0
             # if o>0:
@@ -697,6 +700,7 @@ class LinearRewardTEAQ(BaseReward):
 
             heating_delta_t[z] = max(0, t - t_out) * heating_on
             heating_beyond_comf_delta_t[z] = max(0, t - temp_range[0]) * heating_on
+            heating_service[z] = (min(temp_range[0], t) - t_out)*o
 
         return (
             comfort,
@@ -705,6 +709,7 @@ class LinearRewardTEAQ(BaseReward):
             heating_delta_t,
             heating_beyond_comf_delta_t,
             violation_delta_t,
+            heating_service
         )
 
     def _get_air_quality(
