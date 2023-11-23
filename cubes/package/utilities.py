4a8720cb6f5c5a5c64538ec5148ece57f4d20e76
enjeeneer <scottjeen@gmail.com>
2023-11-23 12:35:45 +0000

    printing

--- cubes/package/utilities.py
+++ cubes/package/utilities.py
@@ -154,10 +154,11 @@ def get_rdd_file(idf: IDF, env_config: EnvConfig):
     # idf.newidfobject("OUTPUT:SURFACES:DRAWING", Report_Type="DXF")
     # delete all other data
     # shutil.rmtree(temp_output_path)
+    with open(temp_output_path + "/eplustbl.htm") as f:
+        test = f
+    print(test)
 
-    table = tablebyname(
-        temp_output_path + "/eplustbl.htm", "Component Sizing Information"
-    )
+    table = tablebyname(test, "Component Sizing Information")
     print(table)
     print(table["Boiler:HotWater"]["MAIN BOILER"]["Design Size Nominal Capacity [W]"])
     # test_data = pd.read_csv(temp_output_path + "/eplusout.csv")
