33ddb90c48a22c99d192504559346d8eddae617f
HannesGauch <hannes.gauch@protonmail.com>
2023-11-23 17:53:38 +0000

    rbc run script

--- exp/hannes/Leiden-study/03_evaluate_controlled/run_rbc_study_t_var.sh
+++ exp/hannes/Leiden-study/03_evaluate_controlled/run_rbc_study_t_var.sh
@@ -1,14 +1,20 @@
 #!/bin/bash
 for tset in 15 20 25
 do
-    for rbc in 0 1 2
+    python3.9 evaluate_rbc.py 0 2022 0 2 $tset 12
+    for c in 5 10
     do
+        let tsb=tset-3
+        python3.9 evaluate_rbc.py $c 2022 0 2 $tset $tsb
+    done
 
-        python3.9 evaluate_rbc.py 0 2022 0 ${rbc} $tset 12
+    for r in {0..10}
+    do
+        python3.9 evaluate_rbc.py 0 2022 $r 0 $tset 12
         for c in 5 10
         do
             let tsb=tset-3
-            python3.9 evaluate_rbc.py $c 2022 0 ${rbc} $tset $tsb
+            python3.9 evaluate_rbc.py $c 2022 $r 0 $tset $tsb
         done
     done
 done
