6ff0e9f7debeedb43c75565fc87f93f451811d6d
enjeeneer <scottjeen@gmail.com>
2023-11-23 12:28:16 +0000

    printing

--- cubes/package/utilities.py
+++ cubes/package/utilities.py
@@ -1,3 +1,4 @@
+# pylint: disable-all
 """collection of utilities for packaging up files for use with gym
 """
 from cubes.constants import package_directory
@@ -6,10 +7,109 @@ from cubes.package.envconfig import EnvConfig
 from pathlib import Path
 import shutil
 from geomeppy import IDF
-from eppy.results import fasthtml
 import pandas as pd
 import numpy as np
 from typing import List
+from io import StringIO
+from eppy.results import readhtml
+
+
+def _decodeline(line, encoding="utf-8"):
+    """decodes bytes to string, if line is not bytes, line is returned
+
+    It will first attempt to decode line with value of `encoding`. If that fails, it will try with encoding="ISO-8859-2". If that fails, it will return line.
+
+    Why is it trying encoding="ISO-8859-2". Looks like E+ uses this encoding in some example files and which is then output in the HTML file
+
+
+
+    Parameters
+    ----------
+    line : str, bytes
+    encoding : str
+
+    Returns
+    -------
+    line : str
+        decoded line
+    """
+    try:
+        return line.decode(encoding)
+    except (AttributeError, UnicodeDecodeError) as e:
+        if e.__class__ == UnicodeDecodeError:
+            # encoding could be ISO-8859-2 in e+ html
+            return _decodeline(line, encoding="ISO-8859-2")
+        else:
+            return line
+
+
+def getnexttable(fhandle):
+    """get the next table in the html file
+
+    Continues to read the file line by line and collects lines from the start of the next table until the end of the table
+
+    Parameters
+    ----------
+    fhandle : file like object
+        A file handle to the E+ HTML table file
+
+    Returns
+    -------
+    table : str
+        The table in HTML format
+    """
+    lines = fhandle
+    tablelines = []
+    for line in lines:
+        line = _decodeline(line)
+        if line.strip().startswith("<table"):
+            tablelines.append(line)
+            break
+    for line in lines:
+        line = _decodeline(line)
+        tablelines.append(line)
+        if line.strip().startswith("</table"):
+            break
+    return "".join(tablelines)
+
+
+def tablebyname(filehandle, header):
+    """fast extraction of the table using the header to identify the table
+
+    This function reads only one table from the HTML file. This is in contrast to `results.readhtml.titletable` that will read all the tables into memory and allows you to interactively look thru them. The function `results.readhtml.titletable` can be very slow on large HTML files.
+
+    This function is useful when you know which file you are looking for. It looks for the title line that is in bold just before the table. Some tables don't have such a title in bold. This function will not work for tables that don't have a title in bold
+
+    Parameters
+    ----------
+    fhandle : file like object
+        A file handle to the E+ HTML table file
+    header: str
+        This is the title of the table you are looking for
+
+    Returns
+    -------
+    titleandtable : (str, list)
+        - (title, table)
+            - title = previous item with a <b> tag
+            - table = rows -> [[cell1, cell2, ..], [cell1, cell2, ..], ..]
+    """
+    htmlheader = f"<b>{header}</b><br><br>"
+
+    with filehandle:
+        for line in filehandle:
+            line = _decodeline(line)
+            if line.strip() == htmlheader:
+                justtable = getnexttable(filehandle)
+                thetable = f"{htmlheader}\n{justtable}"
+                break
+
+    filehandle = StringIO(thetable)
+    htables = readhtml.titletable(filehandle)
+    try:
+        return list(htables[0])
+    except IndexError as e:
+        None
 
 
 def get_rdd_file(idf: IDF, env_config: EnvConfig):
@@ -55,10 +155,10 @@ def get_rdd_file(idf: IDF, env_config: EnvConfig):
     # delete all other data
     # shutil.rmtree(temp_output_path)
 
-    title, table = fasthtml.tablebyname(
+    table = tablebyname(
         temp_output_path + "/eplustbl.htm", "Component Sizing Information"
     )
-    print(title)
+    print(table)
     print(table["Boiler:HotWater"]["MAIN BOILER"]["Design Size Nominal Capacity [W]"])
     # test_data = pd.read_csv(temp_output_path + "/eplusout.csv")
 
