e79041858965e27c3c97051cd2d0f76d5e0cbba7
enjeeneer <scottjeen@gmail.com>
2023-11-24 11:33:39 +0000

    post rebase

--- cubes/cubesgym/utils/rewards.py
+++ cubes/cubesgym/utils/rewards.py
@@ -185,6 +185,7 @@ class ToleranceRewardTEAQ(BaseReward):
         Tolerance based reward function.
         """
         super().__init__(env)
+        print("test")
 
         # get reward related variables (parts of the observation space
         # the agent can influence)  # TODO: emissions?
