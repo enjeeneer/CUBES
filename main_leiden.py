ac768fff723689c9125f797e4bb56e0e75d48a0f
enjeeneer <scottjeen@gmail.com>
2023-11-28 19:03:23 +0000

    action space

--- main_leiden.py
+++ main_leiden.py
@@ -250,6 +250,7 @@ if args.collect_dataset:
 
 observation_length = env.observation_space.shape[0]
 action_length = env.action_space.shape[0]
+print("action space: ", env.action_space)
 
 action_range = [
     env.action_space.low[0],
