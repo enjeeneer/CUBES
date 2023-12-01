16008280b94e751d33eb7b89353eb57a593bf250
HannesGauch <hannes.gauch@protonmail.com>
2023-12-01 09:49:14 +0000

    debug

--- agents/utils.py
+++ agents/utils.py
@@ -244,6 +244,7 @@ def pull_model_from_wandb(
             learnable_temperature=config["learnable_temperature"],
             activation=config["activation"],
             action_range=[np.array(-1), np.array(1)],
+            history_length=config["history_length"]
         )
 
         handshake_agent.critic.load_state_dict(trained_agent.critic.state_dict())
