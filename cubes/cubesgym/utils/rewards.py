2383f39635fea851d3ce90e76aab64da6fb2b803
HannesGauch <hannes.gauch@protonmail.com>
2023-12-03 16:45:01 +0000

    beefy networks; old rewards

--- cubes/cubesgym/utils/rewards.py
+++ cubes/cubesgym/utils/rewards.py
@@ -770,12 +770,12 @@ class LinearRewardTEAQ(BaseReward):
             if o>0:
                 supp = self.thermal_comfort_bonus
             if t < temp_range[0]:
-                comfort += o * (temp_range[0] - t)**2
+                comfort += o * (temp_range[0] - t)
                 t_violation[z] = o
                 violation_delta_t[z] = o * (temp_range[0] - t)
 
             elif t > temp_range[1]:
-                comfort += o * (t - temp_range[1])**2
+                comfort += o * (t - temp_range[1])
                 t_violation[z] = o
                 violation_delta_t[z] = o * (t - temp_range[1])
             else:
