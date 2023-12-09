7d4c7d1b441bd1d0718fc163bd96c53641276bd8
enjeeneer <scottjeen@gmail.com>
2023-12-09 10:18:16 -0600

    remove print

--- agents/workspaces.py
+++ agents/workspaces.py
@@ -562,9 +562,7 @@ class LeidenPEARLWorkspace(LeidenWorkspace):
                 train_metrics = agent.update(replay_buffer=replay_buffer)
 
             eval_metrics = {}
-            if (i % self.eval_frequency == 0) and (
-                i > (agent.batch_size * agent.ensemble_size)
-            ):
+            if (i % self.eval_frequency == 0) and (i > (self.seed_steps)):
                 eval_metrics = self.eval(agent=agent, replay_buffer=replay_buffer)
 
                 if eval_metrics["eval/mean_episode_reward"] > best_eval_reward:
