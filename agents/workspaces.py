15206cc9c45f0ee69cc1072af4460b4f1d8c5c12
enjeeneer <scottjeen@gmail.com>
2024-01-04 16:27:36 +0000

    seeding

--- agents/workspaces.py
+++ agents/workspaces.py
@@ -556,6 +556,12 @@ class LeidenPEARLWorkspace(LeidenWorkspace):
 
             # update models periodically, and after sufficient data has been collected
             train_metrics = {}
+            # update models at end of seed steps
+            print(i, self.seed_steps)
+            if i == self.seed_steps:
+                print("here")
+                train_metrics = agent.update(replay_buffer=replay_buffer)
+
             if (i % self.update_frequency == 0) and (i > (self.seed_steps)):
                 train_metrics = agent.update(replay_buffer=replay_buffer)
 
@@ -706,13 +712,7 @@ class DataCollectionWorkspace:
 
             train_metrics = {}
 
-            # update models at end of seed steps
-            print(i, self.seed_steps)
-            if i == self.seed_steps:
-                print("here")
-                train_metrics = agent.update(replay_buffer=replay_buffer, step=i)
-
-            elif (i % agent.actor_update_frequency == 0) and (i > self.seed_steps):
+            if (i % agent.actor_update_frequency == 0) and (i > self.seed_steps):
                 train_metrics = agent.update(replay_buffer=replay_buffer, step=i)
 
             metrics = {**train_metrics, **eval_metrics}
