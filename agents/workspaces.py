46e916b2562ba9a33c30d1c3c2ecd04f9d16809e
enjeeneer <scottjeen@gmail.com>
2024-01-05 17:41:01 +0000

    sac temp only sub

--- agents/workspaces.py
+++ agents/workspaces.py
@@ -406,6 +406,8 @@ class LeidenSACWorkspace(LeidenWorkspace):
             else:
                 obs = next_obs
 
+            print("obs", self.env.obs_dict)
+
             # sample actions uniformly for seed steps
             if i < self.seed_steps:
                 action = np.random.uniform(
