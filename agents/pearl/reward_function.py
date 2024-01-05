d7e9ea8ce444f3739b84af12f23574a8ac62a2fe
enjeeneer <scottjeen@gmail.com>
2024-01-05 10:23:18 +0000

    remove weighting

--- agents/pearl/reward_function.py
+++ agents/pearl/reward_function.py
@@ -230,6 +230,8 @@ class PEARLRewardFunction:
         """
         trajectories = trajectories.cpu().numpy()
 
+        print("temp indexes: ", self.temperature_idxs)
+
         # Occupancy terms
         occupancy_bools = trajectories[..., self.occupancy_idxs] > 0
         temperatures = np.where(
