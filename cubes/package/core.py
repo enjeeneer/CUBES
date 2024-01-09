b3c81366edfa4071e7c9991f63080f1f910a8c3e
enjeeneer <scottjeen@gmail.com>
2024-01-09 16:59:56 +0000

    obs space

--- cubes/package/core.py
+++ cubes/package/core.py
@@ -127,6 +127,7 @@ def register_environment(
         building_specific_bounds=building_specific_bounds,
     )
     print("ordered obs variables: ", ordered_obs_variables)
+    print("observation space: ", observation_space)
     print(n)  # pylint: disable=undefined-variable
 
     idf.save(filename=env_config.files_dir + "/building_model.idf")
