f173a58b93b5234c09da3d228ed3b1399bc3a6e3
enjeeneer <scottjeen@gmail.com>
2024-01-11 18:35:00 +0000

    renormalised action

--- agents/workspaces.py
+++ agents/workspaces.py
@@ -39,9 +39,11 @@ def transform_sac_battery_action(battery_action: np.ndarray) -> np.ndarray:
     # TODO: check that charging action is the first one
     # charging
     if battery_action >= 0:
-        return np.array([battery_action, -1])  # -1 unnormalises to 0
+        renormalised_battery_action = (2 * (battery_action - (-1)) / (1 - (-1))) - 1
+        return np.array([renormalised_battery_action, -1])  # -1 unnormalises to 0
     else:
-        return np.array([-1, -battery_action])
+        renormalised_battery_action = (2 * (-battery_action - (-1)) / (1 - (-1))) - 1
+        return np.array([-1, renormalised_battery_action])
 
 
 class LeidenWorkspace(AbstractWorkspace):
