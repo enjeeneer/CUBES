a606e621905a1e474ad6f89ee6bcfd96180464ea
enjeeneer <scottjeen@gmail.com>
2024-01-12 12:33:32 +0000

    remove printing

--- agents/workspaces.py
+++ agents/workspaces.py
@@ -484,7 +484,6 @@ class LeidenSACWorkspace(LeidenWorkspace):
                 else:
                     env_action = action
 
-            print("env_action", env_action)
             next_obs, reward, done, _ = self.env.step(env_action)
 
             replay_buffer.add(
